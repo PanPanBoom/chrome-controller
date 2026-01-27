9bb922238a56590428c31e77ffd8396b64a0c9b0
Mehdi HAIMMAD <mehdihaimmad45@gmail.com>
2026-01-27 03:35:24 +0100

    Trying to handle key pressed

--- chrome_controller_extension/background.js
+++ chrome_controller_extension/background.js
@@ -8,7 +8,56 @@ socket.on('connect', () => {
     console.log("Connecté au serveur");
 });
 
+const pressKey = (key) => {
+    const keyCodeMap = {
+        'ArrowLeft': 37,
+        'ArrowUp': 38,
+        'ArrowRight': 39,
+        'ArrowDown': 40,
+        ' ': 32,
+        'f': 70,
+        'Enter': 13,
+        'Tab': 9
+    };
+
+    chrome.tabs.query({ active: true, currentWindow: true}, (tabs) => {
+            if(!tabs[0])
+                return;
+
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                func: (keyName) => {
+                    window.focus();
+
+                    const createEvent = (type) => new KeyboardEvent(type, {
+                        key: keyName,
+                        keyCode: keyCodeMap[keyName],
+                        code: keyName,
+                        which: keyCodeMap[keyName],
+                        bubbles: true,
+                        cancelable: true,
+                        view: window
+                    });
+
+                    const down = createEvent('keydown');
+                    const press = createEvent('keypress');
+                    const up = createEvent('keyup');
+
+                    const target = document.activeElement || document.body
+
+                    target.dispatchEvent(down);
+                    if (keyName === 'Enter' || keyName === ' ') {
+                        target.dispatchEvent(press);
+                    }
+                    target.dispatchEvent(up);
+                },
+                args: [data.key]
+            });
+        });
+}
+
 socket.on('command', (data) => {
+    console.log(data.action);
     if(data.action == 'OPEN_TAB')
     {
         chrome.tabs.query({ active: true, currentWindow: true}, (tabs) => {
@@ -17,5 +66,11 @@ socket.on('command', (data) => {
             else
                 chrome.tabs.create({ url: data.url })
         })
+    }
+
+    else if(data.action == 'KEY_PRESS')
+    {
+        console.log(data.key);
+        pressKey(data.key)
     }
 })
