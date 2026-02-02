75c33be047fca02cfec85f5a3caaaf10f38301c2
Mehdi <mehdihaimmad45@gmail.com>
2026-02-02 03:02:06 +0100

    Added constants through the whole project, upgraded the netflix behaviour, need to move the validate part to only netflix

--- chrome_controller_app/components/ui/Button.tsx
+++ chrome_controller_app/components/ui/Button.tsx
@@ -4,11 +4,12 @@ type ButtonProps = {
     children?: React.ReactNode;
     className?: string;
     onPressOut?: () => void;
+    onLongPress?: () => void;
 }
 
 export const Button = (props: ButtonProps) => {
     return (
-        <Pressable className={`p-3 bg-neutral-200 rounded-xl active:bg-neutral-300 ${props.className != undefined ? props.className : ""}`} onPressOut={props.onPressOut}>
+        <Pressable className={`p-3 bg-neutral-200 rounded-xl active:bg-neutral-300 ${props.className != undefined ? props.className : ""}`} onPressOut={props.onPressOut} onLongPress={props.onLongPress}>
             {props.children}
         </Pressable>
     )
--- chrome_controller_extension/apps.js
+++ chrome_controller_extension/apps.js
@@ -1,15 +1,146 @@
-const apps = {
-    youtube: {
-        rowSelector: "#contents.ytd-rich-grid-renderer",
-        itemsSelector: "a.yt-lockup-view-model__content-image"
-    },
+importScripts("constants.js");
 
-    netflix: {
-        rowSelector: ".tabbed-primary-navigation, .billboard-links, .sliderContent",
-        itemsSelector: "a.slider-refocus, .navigation-tab > a, a.playLink"//".slider-refocus, .playLink"
-    },
+class App
+{
+    constructor(rowsSelector, itemsSelector)
+    {
+        if(this.constructor == App)
+            throw new Error("Abstract classes can't be instantiated.");
 
-    primevideo: {
-
+        this.rowsSelector = rowsSelector;
+        this.itemsSelector = itemsSelector;
     }
+
+    handle(key, tabId)
+    {
+        throw new Error("Must be implemented.");
+    }
+
+    async validate(key, tabId)
+    {
+        const results = await chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            func: () => {
+                document.activeElement.click();
+
+                let stateUpdate = 0;
+                if(document.activeElement.title === "close")
+                    stateUpdate = -1;
+                else if(document.activeElement.className.includes("slider-refocus"))
+                    stateUpdate = 1;
+
+                return stateUpdate;
+            }
+        })
+
+        console.log(results);
+
+        return results[0].result;
+    }
+
+    navigate(key, tabId)
+    {
+        chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            args: [ key.toLowerCase(), this, remoteConstants ],
+            func: (key, selectors, remoteConstants) => {
+                const rows = Array.from(document.querySelectorAll(selectors.rowsSelector));
+                const rowsWithItems = selectors.itemsSelector === "" ? rows.map((row) => [row]) : rows.map((row) => Array.from(row.querySelectorAll(selectors.itemsSelector)));
+                const allItems = selectors.itemsSelector === "" ? rows : Array.from(document.querySelectorAll(selectors.itemsSelector));
+                console.log(rowsWithItems);
+
+                // rowsWithItems[4][3].focus();
+                let x = -1, y = -1;
+
+                rowsWithItems.forEach((row, index) => {
+                    const activeElementIndex = row.indexOf(document.activeElement);
+
+                    if(activeElementIndex >= 0)
+                    {
+                        x = activeElementIndex;
+                        y = index;
+                    }
+                });
+
+                console.log(x + " " + y);
+
+                if(x < 0 || y < 0)
+                    rowsWithItems[0][0].focus();
+                else
+                {
+                    if(key === remoteConstants.DPad.up) {
+                        y--;
+                        x = 0;
+                    } else if(key === remoteConstants.DPad.down){ 
+                        y++;
+                        x = 0;
+                    } else if(key === remoteConstants.DPad.left) x--;
+                    else if(key === remoteConstants.DPad.right) x++;
+
+                    console.log(x + " " + y);
+
+                    rowsWithItems[y][x].focus();
+                }
+
+                document.activeElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+                allItems.forEach(items => items.style.outline = '');
+                document.activeElement.style.outline = "3px solid white";
+                document.activeElement.style.borderRadius = "4px";
+            }
+        })
+    }
+}
+
+class Netflix extends App
+{
+    constructor()
+    {
+        super(".tabbed-primary-navigation, .billboard-links, .sliderContent", "a.slider-refocus, .navigation-tab > a, a.playLink");
+        this.state = 0;
+    }
+
+    handle(key, tabId)
+    {
+        if(key === remoteConstants.DPad.validate)
+            this.validate(key, tabId);
+
+        else
+            this.navigate(key, tabId);
+    }
+
+    async validate(key, tabId)
+    {
+        this.state += await super.validate(key, tabId);
+        console.log(this.state);
+        this.updateSelectors();
+    }
+
+    updateSelectors()
+    {
+        switch(this.state)
+        {
+            case 0:
+                this.rowsSelector = ".tabbed-primary-navigation, .billboard-links, .sliderContent";
+                this.itemsSelector = "a.slider-refocus, .navigation-tab > a, a.playLink";
+                break;
+
+            case 1:
+                this.rowsSelector = ".previewModal-close > span, .previewModal--player-titleTreatment a, .episodeSelector-dropdown button, .episodeSelector-dropdown li, .episode-item"; 
+                this.itemsSelector = ""
+                break;
+        }
+    }
+}
+
+class Youtube extends App
+{
+    constructor()
+    {
+        super("#contents.ytd-rich-grid-renderer", "a.yt-lockup-view-model__content-image");
+    }
+}
+
+const apps = {
+    youtube: new Youtube(),
+    netflix: new Netflix()
 }
