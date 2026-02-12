59ef9b5c5894679b1f29bf64bac4ec0e4789521f
Mehdi <mehdihaimmad45@gmail.com>
2026-02-12 02:02:49 +0100

    new page navigation system

---
+++ chrome_controller_extension/apps/Page.js
@@ -0,0 +1,70 @@
+import { remoteConstants } from "../constants.js";
+
+export class Page
+{
+    constructor(rowsSelector, itemsSelector, identifier)
+    {
+        this.x = -1;
+        this.y = -1;
+        this.rowsSelector = rowsSelector;
+        this.itemsSelector = itemsSelector;
+        this.identifier = identifier;
+    }
+
+    async navigate(key, tabId)
+    {
+        console.log("Debut navigate : " + [this.x, this.y].join(", "));
+        let newX = this.x;
+        let newY = this.y;
+        
+        if(newX < 0 || newY < 0)
+        {
+            newX = 0;
+            newY = 0;
+        }
+
+        else
+        {
+            if(key === remoteConstants.DPad.up) {
+                newY--;
+                newX = 0;
+            } else if(key === remoteConstants.DPad.down){ 
+                newY++;
+                newX = 0;
+            } else if(key === remoteConstants.DPad.left) newX--;
+            else if(key === remoteConstants.DPad.right) newX++;
+        }
+
+        const results = await chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            args: [ this, newX, newY ],
+            func: (platform, x, y) => {
+                const rows = Array.from(document.querySelectorAll(platform.rowsSelector));
+                const rowsWithItems = platform.itemsSelector === "" ? rows.map((row) => [row]) : rows.map((row) => Array.from(row.querySelectorAll(platform.itemsSelector)));
+                const allItems = platform.itemsSelector === "" ? rows : Array.from(document.querySelectorAll(platform.itemsSelector));
+
+                if(y >= rowsWithItems.length || y < 0 || x >= rowsWithItems[y].length || x < 0)
+                    return false;
+
+                const activeElement = rowsWithItems[y][x];
+
+                activeElement.focus();
+                activeElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+                allItems.forEach(items => items.style.outline = '');
+                activeElement.style.outline = "3px solid white";
+                activeElement.style.borderRadius = "4px";
+
+                return true;
+            }
+        });
+
+        if(results[0].result)
+        {
+            this.x = newX;
+            this.y = newY;
+        }
+
+        console.log("Fin navigate : " + [this.x, this.y].join(", "));
+
+    }
+}
