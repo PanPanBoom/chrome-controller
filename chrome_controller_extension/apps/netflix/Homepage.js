e6427749d1230d9cc2e850e4fa2e74e47a76f866
Mehdi <mehdihaimmad45@gmail.com>
2026-02-15 03:23:26 +0100

    upgraded netflix show selection

--- chrome_controller_extension/apps/netflix/Homepage.js
+++ chrome_controller_extension/apps/netflix/Homepage.js
@@ -10,4 +10,62 @@ export class Homepage extends Page
             ".main-header"
         );
     }
+
+    applyNavigation = (selectors, oldX, oldY, newX, newY) =>
+    {
+        const rows = Array.from(document.querySelectorAll(selectors.rowsSelector));
+        const rowsWithItems = selectors.itemsSelector === "" ? rows.map((row) => [row]) : rows.map((row) => Array.from(row.querySelectorAll(selectors.itemsSelector)));
+        const allItems = selectors.itemsSelector === "" ? rows : Array.from(document.querySelectorAll(selectors.itemsSelector));
+
+        let finalX = newX;
+        let finalY = newY;
+
+        if(finalY >= rowsWithItems.length)
+            finalY = 0;
+        else if(finalY < 0)
+            finalY = rowsWithItems.length - 1;
+
+        if(finalX >= rowsWithItems[finalY].length)
+            finalX = rowsWithItems[finalY].length - 1;
+        else if(finalX < 0)
+            finalX = 0;
+
+
+        const activeElementRect = rowsWithItems[finalY][finalX].getBoundingClientRect();
+
+        if(activeElementRect.x + activeElementRect.width > window.innerWidth)
+        {
+            if(oldX < finalX)
+                finalX = rowsWithItems[finalY].length - 1;
+            else
+            {
+                while(finalX > 0 && rowsWithItems[finalY][finalX].getBoundingClientRect().x + rowsWithItems[finalY][finalX].getBoundingClientRect().width > window.innerWidth)
+                    finalX--;
+            }
+        }
+        else if(activeElementRect.x < 0)
+        {
+            if(oldX > finalX)
+                finalX = 0;
+            else
+            {
+                while(finalX < rowsWithItems[finalY].length - 1 && rowsWithItems[finalY][finalX].getBoundingClientRect().x < 0)
+                    finalX++;
+            }
+        }
+
+        const activeElement = rowsWithItems[finalY][finalX];
+
+        if(oldY !== finalY)
+            activeElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+        allItems.forEach(items => items.style.outline = '');
+        activeElement.style.outline = "3px solid white";
+        activeElement.style.borderRadius = "4px";
+
+        return {
+            x: finalX,
+            y: finalY
+        };
+    }
 }
