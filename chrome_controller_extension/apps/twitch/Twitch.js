327ec76c4b100bd874383de8d0013e609ba0d92a
Mehdi <mehdihaimmad45@gmail.com>
2026-02-16 04:26:50 +0100

    (wip): twitch homepage

--- chrome_controller_extension/apps/twitch/Twitch.js
+++ chrome_controller_extension/apps/twitch/Twitch.js
@@ -1,5 +1,6 @@
 import { App } from "../App.js";
 import { Page } from "../Page.js";
+import { Container } from "../Container.js";
 
 export class Twitch extends App
 {
@@ -8,7 +9,11 @@ export class Twitch extends App
         super();
 
         this.pages = [
-            new Page("[data-test-selector='followed-channel']", "", "[data-test-selector='followed-channel']")
+            new Page([
+                new Container("#side-nav", "a, [data-a-target*='side-nav-show']", "column"),
+                new Container("[data-a-target='front-page-carousel']", "[data-a-target='featured-item-left-button'], [data-a-target='featured-item'], [data-a-target='featured-item-right-button']", "row"),
+                new Container(".find-me", "button", "row")
+            ], "#side-nav")
         ]
     }
 }
