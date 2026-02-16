9fd4ea350b780dc8a8236a48c99e72e027c29e87
Mehdi <mehdihaimmad45@gmail.com>
2026-02-16 02:47:38 +0100

    netflix works with new navigation algorithm

--- chrome_controller_extension/apps/netflix/Netflix.js
+++ chrome_controller_extension/apps/netflix/Netflix.js
@@ -3,6 +3,7 @@ import { remoteConstants } from "../../constants.js";
 import { Page } from "../Page.js";
 import { VideoPage } from "./VideoPage.js";
 import { Homepage } from "./Homepage.js";
+import { Container } from "../Container.js";
 
 export class Netflix extends App
 {
@@ -11,9 +12,13 @@ export class Netflix extends App
         super();
 
         this.pages = [
-            new Page([], '.previewModal--container'),
+            new Page([
+                new Container(".previewModal--container", ".previewModal-close > span, .previewModal--player-titleTreatment a, .episodeSelector-dropdown button, .episodeSelector-dropdown li, .episode-item", "column")
+            ], '.previewModal--container'),
             new VideoPage(),
-            new Page([], '.default-ltr-iqcdef-cache-1seef1c'),
+            new Page([
+                new Container(".default-ltr-iqcdef-cache-1cglebk", ".default-ltr-iqcdef-cache-1cglebk a", "row")
+            ], '.default-ltr-iqcdef-cache-1seef1c'),
             new Homepage()
         ];
     }
