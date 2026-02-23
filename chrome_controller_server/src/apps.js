b2dd02dd9580431befdbad9e3e054032758f5492
Mehdi <mehdihaimmad45@gmail.com>
2026-02-23 03:15:16 +0100

    added apps data in server

---
+++ chrome_controller_server/src/apps.js
@@ -0,0 +1,17 @@
+export const apps = [
+    {
+        name: "YouTube",
+        url: "https://www.youtube.com/tv",
+        img: "youtube.png"
+    },
+    {
+        name: "Netflix",
+        url: "https://www.netflix.com/",
+        img: "netflix.png"
+    },
+    {
+        name: "Prime Video",
+        url: "https://www.primevideo.com/",
+        img: "prime.png"
+    }
+]
