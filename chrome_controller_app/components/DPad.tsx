91c6ce0552ddb368ffa8164ed9511f715afb3216
Mehdi <mehdihaimmad45@gmail.com>
2026-02-23 04:34:31 +0100

    added ip in context

--- chrome_controller_app/components/DPad.tsx
+++ chrome_controller_app/components/DPad.tsx
@@ -3,17 +3,19 @@ import { TextButton } from "./ui/TextButton"
 import { ChevronDown, ChevronLeft, ChevronRight, ChevronUp } from "lucide-react-native"
 import { Button } from "./ui/Button"
 import { remoteConstantsDTO } from "@/dtos/remoteConstants"
+import { AppContext } from "@/contexts/appContext"
+import { useContext } from "react"
 
 type DPadProps = {
     commands: remoteConstantsDTO;
-    ip: string;
 }
 
 export const DPad = (props: DPadProps) => {
+    const { serverIp } = useContext(AppContext);
     const buttonStyle = "w-[30%] h-20 flex justify-center items-center";
 
     const handleValidation = () => {
-        fetch(`${props.ip}/extension/keypress`, {
+        fetch(`${serverIp}/extension/keypress`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -33,13 +35,13 @@ export const DPad = (props: DPadProps) => {
 
     return (
         <View className={`flex justify-center items-center w-full gap-${gap}`}>
-            <DPadArrow ip={props.ip} keySimulated={props.commands.DPad.up} className={buttonStyle} arrows={arrowsMapping}/>
+            <DPadArrow keySimulated={props.commands.DPad.up} className={buttonStyle} arrows={arrowsMapping}/>
             <View className={`flex flex-row gap-${gap}`}>
-                <DPadArrow ip={props.ip} keySimulated={props.commands.DPad.left} className={buttonStyle} arrows={arrowsMapping} />
+                <DPadArrow keySimulated={props.commands.DPad.left} className={buttonStyle} arrows={arrowsMapping} />
                 <Button className={buttonStyle} onPressOut={handleValidation}/>
-                <DPadArrow ip={props.ip} keySimulated={props.commands.DPad.right} className={buttonStyle} arrows={arrowsMapping} />
+                <DPadArrow keySimulated={props.commands.DPad.right} className={buttonStyle} arrows={arrowsMapping} />
             </View>
-            <DPadArrow ip={props.ip} keySimulated={props.commands.DPad.down} className={buttonStyle} arrows={arrowsMapping}/>
+            <DPadArrow keySimulated={props.commands.DPad.down} className={buttonStyle} arrows={arrowsMapping}/>
         </View>
     )
 }
@@ -48,12 +50,13 @@ type DPadArrowProps = {
     keySimulated: string;
     className?: string;
     arrows: Record<string, React.ReactNode>;
-    ip: string;
 }
 
 const DPadArrow = (props: DPadArrowProps) => {
+    const { serverIp } = useContext(AppContext);
+
     const handleClick = () => {
-        fetch(`${props.ip}/extension/keypress`, {
+        fetch(`${serverIp}/extension/keypress`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
