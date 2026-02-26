cc9a03b4a68561004b1ef35b9f89929c49a4d026
Mehdi <mehdihaimmad45@gmail.com>
2026-02-26 09:56:53 +0100

    added color to active tab

--- chrome_controller_app/app/(tabs)/apps.tsx
+++ chrome_controller_app/app/(tabs)/apps.tsx
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 export default function Apps()
 {
     return (
-        <View>
+        <View className="bg-background flex-1">
             <AppsSection />
         </View>
     )
