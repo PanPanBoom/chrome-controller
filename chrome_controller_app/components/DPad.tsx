1faf9b4302bb87996af091853a2f503888fe4261
Mehdi <mehdihaimmad45@gmail.com>
2026-02-27 02:11:02 +0100

    stylised DPad

--- chrome_controller_app/components/DPad.tsx
+++ chrome_controller_app/components/DPad.tsx
@@ -1,11 +1,12 @@
-import { View } from "react-native"
+import { Pressable, TouchableOpacity, View, Text } from "react-native"
 import { TextButton } from "./ui/TextButton"
-import { ChevronDown, ChevronLeft, ChevronRight, ChevronUp } from "lucide-react-native"
+import { ChevronDown, ChevronLeft, ChevronRight, ChevronUp, LucideIcon } from "lucide-react-native"
 import { Button } from "./ui/Button"
 import { remoteConstantsDTO } from "@/dtos/remoteConstants"
 import { AppContext } from "@/contexts/appContext"
-import { useContext } from "react"
+import { JSX, useContext } from "react"
 import { sendKeyPress } from "@/server/socket"
+import { colors } from "@/constants/colors"
 
 type DPadProps = {
     commands: remoteConstantsDTO;
@@ -13,42 +14,65 @@ type DPadProps = {
 
 export const DPad = (props: DPadProps) => {
     const { serverIp } = useContext(AppContext);
-    const buttonStyle = "w-[30%] h-20 flex justify-center items-center";
-    
-    const arrowsMapping: Record<string, React.ReactNode> = {
-        [props.commands.DPad.up]: (<ChevronUp />),
-        [props.commands.DPad.left]: (<ChevronLeft />),
-        [props.commands.DPad.right]: (<ChevronRight />),
-        [props.commands.DPad.down]: (<ChevronDown />)
-    }
-
-    const gap = 2;
 
     return (
-        <View className={`flex justify-center items-center w-full gap-${gap}`}>
-            <DPadArrow keySimulated={props.commands.DPad.up} className={buttonStyle} arrows={arrowsMapping}/>
-            <View className={`flex flex-row gap-${gap}`}>
-                <DPadArrow keySimulated={props.commands.DPad.left} className={buttonStyle} arrows={arrowsMapping} />
-                <Button className={`${buttonStyle} bg-primary`} onPressOut={() => sendKeyPress(serverIp, props.commands.DPad.validate)}/>
-                <DPadArrow keySimulated={props.commands.DPad.right} className={buttonStyle} arrows={arrowsMapping} />
+        <View className="w-[55%] aspect-square border-primary border border-[5px] p-5 rounded-full bg-background" style={{ shadowColor: colors.primary, shadowOffset: { width: 0, height: 0 }, shadowOpacity: 0.8, shadowRadius: 15 }}>
+            <View className="flex relative rounded-full p-2 relative aspect-square" style={{transform: [{ rotate: '45deg' }]}}>
+                <DPadButton keySimulated={props.commands.DPad.up} commands={props.commands}/>
+                <DPadButton keySimulated={props.commands.DPad.left} commands={props.commands} />
+                <DPadButton keySimulated={props.commands.DPad.right} commands={props.commands} />
+                <DPadButton keySimulated={props.commands.DPad.down} commands={props.commands} />
+                <Button
+                    className={`bg-primary rounded-full m-auto w-[40%] aspect-square`}
+                    onPressOut={() => sendKeyPress(serverIp, props.commands.DPad.validate)}
+                    style={{transform: [{rotate: '-45deg'}]}}
+                >
+                    <Text style={{color: colors.text}}>OK</Text>
+                </Button>
             </View>
-            <DPadArrow keySimulated={props.commands.DPad.down} className={buttonStyle} arrows={arrowsMapping}/>
         </View>
     )
 }
 
 type DPadArrowProps = {
     keySimulated: string;
-    className?: string;
-    arrows: Record<string, React.ReactNode>;
+    commands: remoteConstantsDTO;
 }
 
-const DPadArrow = (props: DPadArrowProps) => {
+type ArrowConfig = {
+    component: LucideIcon;
+    className: string;
+}
+
+const DPadButton = (props: DPadArrowProps) => {
     const { serverIp } = useContext(AppContext);
 
+    const arrowsMapping: Record<string, ArrowConfig> = {
+        [props.commands.DPad.up]: {
+            component: ChevronUp,
+            className: "left-0 top-0 rounded-tl-full"
+        },
+        [props.commands.DPad.left]: {
+            component: ChevronLeft,
+            className: "left-0 bottom-0 rounded-bl-full"
+        },
+        [props.commands.DPad.right]: {
+            component: ChevronRight,
+            className: "right-0 top-0 rounded-tr-full"
+        },
+        [props.commands.DPad.down]: {
+            component: ChevronDown,
+            className: "right-0 bottom-0 rounded-br-full"
+        }
+    }
+
+    const Component = arrowsMapping[props.keySimulated].component;
+
     return (
-        <Button className={props.className} onPressOut={() => sendKeyPress(serverIp, props.keySimulated)}>
-            { props.arrows[props.keySimulated] }
+        <Button className={`${arrowsMapping[props.keySimulated].className} w-[50%] h-[50%] absolute bg-background-light`} onPressOut={() => sendKeyPress(serverIp, props.keySimulated)}>
+            <View className="bg-background-hover rounded-full p-1">
+                <Component style={{transform: [{ rotate: '-45deg' }]}} color={colors.text}/>
+            </View>
         </Button>
     )
 }
