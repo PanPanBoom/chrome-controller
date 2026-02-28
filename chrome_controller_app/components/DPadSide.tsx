0201e8765e8f2a2fa94baca4ad9e6acbac371996
Mehdi <mehdihaimmad45@gmail.com>
2026-02-28 05:44:52 +0100

    added DPadSide

---
+++ chrome_controller_app/components/DPadSide.tsx
@@ -0,0 +1,59 @@
+import { colors } from "@/constants/colors";
+import { useState } from "react";
+import { StyleSheet, View, ViewProps } from "react-native";
+import Svg, { Path, G } from 'react-native-svg';
+import { CurvedPanel, polarToCartesian } from "./ui/CurvedPanel";
+import { LucideIcon } from "lucide-react-native";
+
+type DPadSideProps = ViewProps & {
+    startAngle: number;
+    endAngle: number;
+    icons: {
+        icon: LucideIcon;
+        onPress: () => void;
+    }[];
+}
+
+export const DPadSide = (props: DPadSideProps) => {
+    const [activeIndex, setActiveIndex] = useState(-1);
+    const size = 425;
+    const iconSize = 20;
+    const innerR = size * 0.3;
+    const outerR = size * 0.37;
+
+    const step = Math.abs(props.endAngle - props.startAngle) / 3;
+
+    const sections = [
+        { startAngle: props.startAngle, endAngle: props.startAngle + step },
+        { startAngle: props.startAngle + step, endAngle: props.endAngle - step },
+        { startAngle: props.endAngle - step, endAngle: props.endAngle}
+    ];
+
+    const buildIcon = (Icon: LucideIcon, coordinates: {x: number, y: number}, key: number) => <Icon key={key} size={iconSize} color={colors.text} style={{position: "absolute", top: coordinates.y - iconSize/2, left: coordinates.x - iconSize/2}}/>
+
+    const iconCoordinates = sections.map(section => polarToCartesian(size/2, size/2, (innerR + outerR)/2, (section.startAngle + section.endAngle)/2));
+
+    return (
+        <View {...props}>
+            <Svg width={size} height={size}>
+                {
+                    sections.map((section, index) => <CurvedPanel
+                                                        key={index}
+                                                        size={size}
+                                                        startAngle={section.startAngle}
+                                                        endAngle={section.endAngle}
+                                                        innerR={innerR}
+                                                        outerR={outerR}
+                                                        onPressIn={() => setActiveIndex(index)}
+                                                        onPressOut={() => setActiveIndex(-1)}
+                                                        fill={activeIndex === index ? colors.backgroundHover : colors.backgroundLight}
+                                                        onPress={props.icons[index].onPress}
+                                                        />)
+                }
+            </Svg>
+            {
+                props.icons.map((iconData, index) => buildIcon(iconData.icon, iconCoordinates[index], index))
+            }
+        </View>
+    );
+}
