ab3cc179da384ce2c99debc9b9e6cb848b981f05
Mehdi <mehdihaimmad45@gmail.com>
2026-03-05 23:41:51 +0100

    exported scannetwork function to utils, to prepare cast screen

--- chrome_controller_app/etc/utils.ts
+++ chrome_controller_app/etc/utils.ts
@@ -1,4 +1,43 @@
 import clsx, { ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import * as Network from 'expo-network';
+import { sendPing } from '@/server/socket';
 
 export const cn = (...classes: ClassValue[]) => twMerge(clsx(...classes));
+
+export const scanNetwork = async () => {
+    const ipAddress = await Network.getIpAddressAsync();
+    const baseIp = ipAddress.substring(0, ipAddress.lastIndexOf('.'));
+    
+    console.log(`Scanning network ${baseIp}.x ...`);
+
+    const promises = [];
+    for(let i = 1; i <= 254; i++)
+    {
+      const targetIp = `${baseIp}.${i}`;
+
+      const controller = new AbortController();
+      const timeoutId = setTimeout(() => controller.abort(), 900);
+
+      promises.push(
+        sendPing(targetIp, controller.signal)
+          .then(res => res.json())
+          .then(data => {
+            if(data)
+              return {
+                ip: targetIp,
+                serverData: data
+              };
+            return null;
+          })
+          .catch(() => null)
+      );
+    }
+
+    const results = await Promise.all(promises);
+    const foundServers = results.filter(server => server !== null);
+
+    console.log('Serveurs trouvés :', foundServers);
+
+    return foundServers;
+  }
