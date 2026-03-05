64e5e1f2085f7608203be3e7c651ec9318840f8f
Mehdi <mehdihaimmad45@gmail.com>
2026-03-05 23:43:40 +0100

    started developing scan network component

--- chrome_controller_app/app/(tabs)/cast.tsx
+++ chrome_controller_app/app/(tabs)/cast.tsx
@@ -1,11 +1,12 @@
+import { NetworkScanRadar } from "@/components/NetworkScanRadar";
 import { CustomText } from "@/components/ui/CustomText";
 import { Screen } from "@/components/ui/Screen";
 
 export default function Cast()
 {
     return (
-        <Screen>
-            <CustomText>Cast</CustomText>
+        <Screen className="justify-center items-center">
+            <NetworkScanRadar className="w-[90%]"/>
         </Screen>
     )
 }
---
+++ chrome_controller_app/components/NetworkScanRadar.tsx
@@ -0,0 +1,38 @@
+import { Pressable, ViewProps, View } from "react-native"
+import { Radar } from "./ui/Radar"
+import { useEffect, useState } from "react"
+import { scanNetwork } from "@/etc/utils";
+import { Button } from "./ui/Button";
+import { CustomText } from "./ui/CustomText";
+import { Computer } from "lucide-react-native";
+import { colors } from "@/constants/colors";
+import { NetworkScanServer } from "./NetworkScanServer";
+import { ServerDataDTO } from "@/dtos/serverData";
+
+export const NetworkScanRadar = (props: ViewProps) => {
+    const [isScanning, setScanning] = useState(false);
+    const [servers, setServers] = useState<ServerDataDTO[]>([]);
+
+    const launchScan = () => {
+        setServers([]);
+        setScanning(true);
+        scanNetwork().then(foundServers => {
+            setServers(foundServers);
+            setScanning(false);
+        });
+    }
+
+    return (
+        <View className="gap-10">
+            <Radar active={isScanning} {...props}>
+                {
+                    servers.map((server, index) => 
+                        <NetworkScanServer key={index} server={server} className=""/>)
+                }
+            </Radar>
+            <Button onPress={launchScan} className="bg-primary">
+                <CustomText>{isScanning ? "Scan en cours" : "Lancer un scan"}</CustomText>
+            </Button>
+        </View>
+    )
+}
