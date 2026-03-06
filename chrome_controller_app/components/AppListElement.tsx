b4d69ebd28225838d011c6b4fd274d6f5ff082e8
Mehdi <mehdihaimmad45@gmail.com>
2026-03-06 02:36:47 +0100

    fixed app redirection

--- chrome_controller_app/components/AppListElement.tsx
+++ chrome_controller_app/components/AppListElement.tsx
@@ -1,5 +1,5 @@
 import { App } from "@/dtos/app"
-import { Pressable, Image } from "react-native";
+import { Pressable, Image, Linking } from "react-native";
 import { CustomText } from "./ui/CustomText";
 import { useContext } from "react";
 import { AppContext } from "@/contexts/appContext";
@@ -11,10 +11,15 @@ type AppListElementProps = {
 
 export const AppListElement = (props: AppListElementProps) => {
     const { serverIp } = useContext(AppContext);
-    console.log(`http://${serverIp}:3000${props.app.img}`);
+
+    const handleClick = () => {
+        sendAppLaunch(serverIp, props.app.url);
+        if(props.app.redirect)
+            Linking.openURL(props.app.redirect);
+    }
     
     return (
-        <Pressable className="flex-row flex-1 items-center rounded-full bg-background-light gap-2" onPress={() => sendAppLaunch(serverIp, props.app.url)}>
+        <Pressable className="flex-row flex-1 items-center rounded-full bg-background-light gap-2" onPress={handleClick}>
             <Image source={{ uri: `http://${serverIp}:3000${props.app.img}`}} className="h-full aspect-square rounded-full"/>
             <CustomText className="my-4 text-xl">{props.app.name}</CustomText>
         </Pressable>
