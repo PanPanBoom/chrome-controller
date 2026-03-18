61c61cb6944bd62305bc270bae7a2a943dc1391b
Mehdi <mehdihaimmad45@gmail.com>
2026-03-18 20:52:08 +0100

    reviewed show design in carousel

--- chrome_controller_app/components/Show.tsx
+++ chrome_controller_app/components/Show.tsx
@@ -1,4 +1,4 @@
-import { Image, Pressable, View, ViewProps } from "react-native"
+import { Image, Pressable, ScrollView, View, ViewProps } from "react-native"
 import { CustomText } from "./ui/CustomText"
 import { Button } from "./ui/Button"
 import { BlurView } from "expo-blur"
@@ -12,6 +12,7 @@ import { sendAppLaunch } from "@/server/socket"
 import { useContext, useState } from "react"
 import { AppContext } from "@/contexts/appContext"
 import { BlurredIconButton } from "./ui/BlurredIconButton"
+import { LinearGradient } from 'expo-linear-gradient';
 
 type ShowProps = ViewProps & {
     data: ShowDTO;
@@ -39,25 +40,37 @@ export const Show = ({className, ...props}: ShowProps) => {
     );
 
     return (
-        <View className={cn("w-full justify-center items-center border border-background-hover bg-background-hover rounded-xl aspect-video overflow-hidden gap-2", className)} {...props}>
+        <View className={cn("w-full justify-center items-center border border-background-hover bg-background-hover rounded-xl aspect-video overflow-hidden", className)} {...props}>
             {
-                props.data.img.includes('svg') ?
-                <>
-                    { infoSection }
-                    { carouselButtons }
-                    { castButton }
-                </> : showInfos ? 
-                <>
-                    { infoSection }
-                    { carouselButtons }
-                    { infoButton }
-                </> :
-                <>
-                    <Image source={{uri: props.data.img}} className="w-full aspect-video absolute"/>
-                    { carouselButtons }
-                    { infoButton }
-                    { castButton }
-                </>
+                props.data.img.includes('svg') == false &&
+                <Image source={{uri: props.data.img}} className="w-full aspect-video absolute" style={{opacity: showInfos ? 0.25 : 1}}/>
+            }
+            {
+                props.data.title !== "" &&
+                <LinearGradient colors={['rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0.8)']} style={{position: "absolute", bottom: 0, left: 0, width: '100%', height: '50%'}} />
+            }
+            <Pressable className="w-1/2 h-full absolute left-0" onPress={props.onPrev}/>
+            <Pressable className="w-1/2 h-full absolute right-0" onPress={props.onNext}/>
+            <View className="flex-row flex-1">
+                {
+                    showInfos ?
+                    <ScrollView showsVerticalScrollIndicator={false}>
+                        <CustomText className="p-2 text-justify">{props.data.overview}</CustomText>
+                    </ScrollView> :
+                    <View className="flex-1" pointerEvents="none"/>
+                }
+                <View className="pt-2 pr-2">
+                    <BlurredIconButton icon={Info} color={colors.text} iconSize={20} blurViewClassName="p-1" className="" onPress={() => setShowInfos(prev => !prev)}/>
+                </View>
+            </View>
+            <ScrollView horizontal className="grow-0 p-2 w-full" showsHorizontalScrollIndicator={false}>   
+                <CustomTitle>{props.data.title}</CustomTitle>
+            </ScrollView>
+            {
+                showInfos == false &&
+                <View className="absolute w-full h-full justify-center items-center">
+                    <BlurredIconButton icon={Cast} color={colors.text} onPress={() => sendAppLaunch(server.ip, props.data.link)}/>
+                </View>
             }
         </View>
     )
