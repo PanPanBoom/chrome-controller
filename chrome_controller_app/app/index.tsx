d9a4a87bc7adab056f5748e857b07c8c5897dfb7
Mehdi <mehdihaimmad45@gmail.com>
2026-03-23 12:24:20 +0100

    now redirects to cast screen when no server found on app launch

--- chrome_controller_app/app/index.tsx
+++ chrome_controller_app/app/index.tsx
@@ -1,50 +1,36 @@
-import { View, Text, ActivityIndicator } from "react-native";
-import * as Network from 'expo-network';
-import { useContext, useEffect, useState } from "react";
+import { ActivityIndicator } from "react-native";
+import { useContext, useEffect } from "react";
 import { useRouter } from "expo-router";
-import { Button } from "@/components/ui/Button";
 import { AppContext } from "@/contexts/appContext";
-import { sendPing } from "@/server/socket";
 import { Screen } from "@/components/ui/Screen";
 import { CustomText } from "@/components/ui/CustomText";
 import { scanNetwork } from "@/etc/utils";
 import { ServerDataDTO } from "@/dtos/serverData";
 
 export default function Index() {
-  const [loading, setLoading] = useState(true);
   const router = useRouter();
   const { server, setServer } = useContext(AppContext);
 
-  const launchScanNetwork = async () => {
-    setLoading(true);
-    const foundServers: ServerDataDTO[] = await scanNetwork();
-    setLoading(false);
-    if(foundServers.length > 0)
-    {
-      setServer(foundServers[0]);
-      router.push({ pathname: '/(tabs)/remote' });
-    }
-  }
-
   useEffect(() => {
+    const launchScanNetwork = async () => {
+      const foundServers: ServerDataDTO[] = await scanNetwork();
+      if(foundServers.length > 0)
+      {
+        setServer(foundServers[0]);
+        router.push({ pathname: '/(tabs)/remote' });
+      }
+
+      else
+        router.push({ pathname: '/(tabs)/cast'});
+    }
+
     launchScanNetwork();
   }, []);
 
   return (
     <Screen className="flex justify-center items-center gap-4">
-      {
-        loading ?
-        <>
-          <ActivityIndicator />
-          <CustomText>Recherche de serveurs...</CustomText>
-        </> :
-        <>
-          <CustomText>Aucun serveur trouvé</CustomText>
-          <Button onPressOut={launchScanNetwork} className="w-[75%]">
-            <CustomText>Rechercher</CustomText>
-          </Button>
-        </>
-      }
+        <ActivityIndicator />
+        <CustomText>Recherche de serveurs...</CustomText>
     </Screen>
   );
 }
