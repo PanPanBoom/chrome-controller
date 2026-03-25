353e69927da9923fe72dae96175313f62a290ef4
Mehdi <mehdihaimmad45@gmail.com>
2026-03-25 04:54:53 +0100

    design change in screen

--- chrome_controller_app/components/ui/Screen.tsx
+++ chrome_controller_app/components/ui/Screen.tsx
@@ -1,10 +1,21 @@
+import { colors } from "@/constants/colors";
 import { cn } from "@/etc/utils";
-import { View, ViewProps } from "react-native";
+import { BlurView } from "expo-blur";
+import { Dimensions, View, ViewProps } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-export const Screen = ({className, ...props}: ViewProps) => {
+export const Screen = ({className, style, ...props}: ViewProps) => {
+    const insets = useSafeAreaInsets();
+    const { width, height } = Dimensions.get('window');
+
+    const offset = 100;
+
     return (
-        <View className={cn("px-8 py-4 bg-background flex-1", className)} {...props}>
-            {props.children}
+        <View className="flex-1 bg-background">
+            <View className="bg-background aspect-square w-[50%] shadow absolute" style={{shadowColor: colors.primary, shadowRadius: 130, shadowOffset: {width: -offset, height: offset}, left: width - 1, bottom: height - 1}}/>
+            <View className={cn("px-8 py-4 flex-1", className)} style={[{paddingTop: insets.top}, style]} {...props}>
+                {props.children}
+            </View>
         </View>
     )
 }
