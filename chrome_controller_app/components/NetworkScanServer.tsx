e3524bb72b217c9f09935f4ecd47308f7026cc76
Mehdi <mehdihaimmad45@gmail.com>
2026-07-21 04:10:18 +0200

    pairing with tv working

--- chrome_controller_app/components/NetworkScanServer.tsx
+++ chrome_controller_app/components/NetworkScanServer.tsx
@@ -3,18 +3,21 @@ import { ServerDataDTO } from "@/dtos/serverData"
 import { Computer } from "lucide-react-native"
 import { Animated, Image, Pressable, PressableProps, StyleSheet, useAnimatedValue, ViewProps } from "react-native"
 import { CustomText } from "./ui/CustomText"
-import { useEffect } from "react"
+import { useContext, useEffect } from "react"
 import { cn } from "@/etc/utils"
 import { router } from "expo-router"
 import { Button } from "./ui/Button"
+import { ModalContext } from "@/contexts/modalProvider"
+import { ServerDataWidget } from "./ServerDataWidget"
 
 type NetworkScanServer = PressableProps & {
     server: ServerDataDTO;
 }
 
 export const NetworkScanServer = ({className, ...props}: NetworkScanServer) => {
+    const { showModal } = useContext(ModalContext);
+    
     const fadeAnimation = useAnimatedValue(0);
-
 
     useEffect(() => {
         Animated.timing(fadeAnimation, {
@@ -25,7 +28,7 @@ export const NetworkScanServer = ({className, ...props}: NetworkScanServer) => {
     }, []);
 
     return (
-        <Button className={cn("absolute w-[13%] bg-transparent p-0 active:bg-transparent", className)} onPress={() => router.push({ pathname: '/modal', params: { server: JSON.stringify(props.server)} })} {...props}>
+        <Button className={cn("absolute w-[13%] bg-transparent p-0 active:bg-transparent", className)} onPress={() => showModal(<ServerDataWidget server={props.server}/>)} {...props}>
             <Animated.View className="flex-1 justify-center items-center" style={{opacity: fadeAnimation.interpolate({
                 inputRange: [0, 100],
                 outputRange: [0, 1]
