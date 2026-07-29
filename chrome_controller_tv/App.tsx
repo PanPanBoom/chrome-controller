ea6385d5753d3f6efc0d934e690074673679ef1c
Mehdi <mehdihaimmad45@gmail.com>
2026-07-29 03:43:01 +0200

    cast from remote to tv working

--- chrome_controller_tv/App.tsx
+++ chrome_controller_tv/App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import { StatusBar, StyleSheet, useColorScheme, View, Text } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme, View, Text, Linking } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
@@ -13,6 +13,8 @@ import {
 import "./global.css";
 import { CustomText } from './components/CustomText';
 import { CustomTitle } from './components/CustomTitle';
+import Video from 'react-native-video';
+import { useEffect, useState } from 'react';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -27,17 +29,40 @@ function App() {
 
 function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
+  const [videoUrl, setVideoUrl] = useState("");
 
-  return (
-    <View className='flex bg-background flex-1 justify-center items-center gap-2'>
-      <CustomTitle>Bienvenue sur Chrome Controller TV !</CustomTitle>
-      <CustomText>Castez un film depuis l'application mobile pour le lancer !</CustomText>
-      {/* <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      /> */}
-    </View>
-  );
+  useEffect(() => {
+    Linking.addEventListener('url', ({ url }) => {
+      const params = new URL(url);
+
+      setVideoUrl(params.searchParams.get('url') ?? "");
+    })
+  }, []);
+
+  return videoUrl == "" ?
+        <View className='flex bg-background flex-1 justify-center items-center gap-2'>
+          <CustomTitle>Bienvenue sur Chrome Controller TV !</CustomTitle>
+          <CustomText>Castez un film depuis l'application mobile pour le lancer !</CustomText>
+        </View> :
+        <Video
+          source={{
+            uri: videoUrl,
+            headers: {
+              Referer: 'https://noxpulse.cc/'
+            },
+            bufferConfig: {
+              minBufferMs: 30000,
+              maxBufferMs: 60000,
+              bufferForPlaybackMs: 5000,
+              bufferForPlaybackAfterRebufferMs: 8000,
+            }
+          }}
+          style={{width: '100%', height: '100%'}}
+          controls={true}
+          resizeMode='contain'
+          reportBandwidth={true}
+          fullscreen={true}
+        />
 }
 
 export default App;
