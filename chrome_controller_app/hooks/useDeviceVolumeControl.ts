fe12915c8d76b02da396f58e10fdd8753c0d5330
Mehdi <mehdihaimmad45@gmail.com>
2026-09-18 19:27:07 +0200

    fixed mobile app crash

--- chrome_controller_app/hooks/useDeviceVolumeControl.ts
+++ chrome_controller_app/hooks/useDeviceVolumeControl.ts
@@ -1,29 +1,40 @@
 import { remoteConstantsDTO } from "@/dtos/remoteConstants";
 import { sendKeyPress } from "@/server/socket";
 import { useEffect, useRef } from "react";
+import { EmitterSubscription } from "react-native";
 import { VolumeManager } from "react-native-volume-manager";
 
 export const useDeviceVolumeControl = (ip: string, constants: remoteConstantsDTO) => {
     const prevVolume = useRef(0.5);
 
     useEffect(() => {
-        VolumeManager.showNativeVolumeUI({ enabled: false });
-        VolumeManager.setVolume(0.5);
+        let volumeListener: EmitterSubscription;
 
-        const volumeListener = VolumeManager.addVolumeListener((result) => {
-            const currentVolume = result.volume;
-
-            sendKeyPress(ip, currentVolume > prevVolume.current ? constants.volume.up : constants.volume.down, constants.directions.shortPress);
-
-            prevVolume.current = currentVolume;
-            if(currentVolume >= 0.8 || currentVolume <= 0.2){
-                VolumeManager.setVolume(0.5);
-                prevVolume.current = 0.5;
+        const timer = setTimeout(async () => {
+            try {
+                VolumeManager.showNativeVolumeUI({ enabled: false });
+    
+                await VolumeManager.setVolume(0.5);
+        
+                volumeListener = VolumeManager.addVolumeListener((result) => {
+                    const currentVolume = result.volume;
+        
+                    sendKeyPress(ip, currentVolume > prevVolume.current ? constants.volume.up : constants.volume.down, constants.directions.shortPress);
+        
+                    prevVolume.current = currentVolume;
+                    if(currentVolume >= 0.8 || currentVolume <= 0.2){
+                        VolumeManager.setVolume(0.5);
+                        prevVolume.current = 0.5;
+                    }
+                });
+            } catch(err) {
+                console.log('VolumeManager error:' + err);
             }
-        });
+        }, 300);
 
         return () => {
-            volumeListener.remove();
+            clearTimeout(timer);
+            volumeListener?.remove();
             VolumeManager.showNativeVolumeUI({ enabled: true });
         }
     })
